547d61e2a127fb7e4394ae38951484ee5acda4b4
FelixAllard <xilef992@gmail.com>
2024-06-16 01:04:29 -0400

    Startedd integrating state based ai ssytem

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -16,17 +16,10 @@ namespace SCP3199 {
 
         private void Awake() {
             Logger = base.Logger;
-
-            // If you don't want your mod to use a configuration file, you can remove this line, Configuration.cs, and other references.
+            
             BoundConfig = new PluginConfig(base.Config);
-
-            // This should be ran before Network Prefabs are registered.
+            
             InitializeNetworkBehaviours();
-
-            // We load the asset bundle that should be next to our DLL file, with the specified name.
-            // You may want to rename your asset bundle from the AssetBundle Browser in order to avoid an issue with
-            // asset bundle identifiers being the same between multiple bundles, allowing the loading of only one bundle from one mod.
-            // In that case also remember to change the asset bundle copying code in the csproj.user file.
             var bundleName = "modassets";
             ModAssets = AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(Info.Location), bundleName));
             if (ModAssets == null) {
@@ -38,30 +31,6 @@ namespace SCP3199 {
             var ExampleEnemy = ModAssets.LoadAsset<EnemyType>("SCP3199");
             var ExampleEnemyTN = ModAssets.LoadAsset<TerminalNode>("ExampleEnemyTN");
             var ExampleEnemyTK = ModAssets.LoadAsset<TerminalKeyword>("ExampleEnemyTK");
-
-            // Optionally, we can list which levels we want to add our enemy to, while also specifying the spawn weight for each.
-            /*
-            var ExampleEnemyLevelRarities = new Dictionary<Levels.LevelTypes, int> {
-                {Levels.LevelTypes.ExperimentationLevel, 10},
-                {Levels.LevelTypes.AssuranceLevel, 40},
-                {Levels.LevelTypes.VowLevel, 20},
-                {Levels.LevelTypes.OffenseLevel, 30},
-                {Levels.LevelTypes.MarchLevel, 20},
-                {Levels.LevelTypes.RendLevel, 50},
-                {Levels.LevelTypes.DineLevel, 25},
-                // {Levels.LevelTypes.TitanLevel, 33},
-                // {Levels.LevelTypes.All, 30},     // Affects unset values, with lowest priority (gets overridden by Levels.LevelTypes.Modded)
-                {Levels.LevelTypes.Modded, 60},     // Affects values for modded moons that weren't specified
-            };
-            // We can also specify custom level rarities
-            var ExampleEnemyCustomLevelRarities = new Dictionary<string, int> {
-                {"EGyptLevel", 50},
-                {"46 Infernis", 69},    // Either LLL or LE(C) name can be used, LethalLib will handle both
-            };
-            */
-
-            // Network Prefabs need to be registered. See https://docs-multiplayer.unity3d.com/netcode/current/basics/object-spawning/
-            // LethalLib registers prefabs on GameNetworkManager.Start.
             NetworkPrefabs.RegisterNetworkPrefab(ExampleEnemy.enemyPrefab);
 
             // For different ways of registering your enemy, see https://github.com/EvaisaDev/LethalLib/blob/main/LethalLib/Modules/Enemies.cs
---
+++ src/SCP3199/SCP3199Ai.cs
@@ -0,0 +1,489 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using GameNetcodeStuff;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace SCP3199.SCP3199;
+
+class SCP3199AI : ModEnemyAI<SCP3199AI>
+{
+    // We use this list to destroy loaded game objects when plugin is reloaded
+    internal static List<GameObject> SCP682Objects = [];
+    
+    
+
+    public enum Speed
+    {
+        Walking = 4,
+        Running = 6
+    }
+
+    public void SetAgentSpeed(Speed speed)
+    {
+        agent.speed = (int)speed;
+
+        bool newIsRunning = speed == Speed.Running;
+        if (creatureAnimator.GetBool(Anim.isRunning) != newIsRunning)
+            creatureAnimator.SetBool(Anim.isRunning, newIsRunning);
+    }
+    static class Anim
+    {
+        // do: trigger
+        // is: boolean
+        internal const string doKillEnemy = "KillEnemy"; // base game thing, gets called automatically
+        internal const string isWalking = "isWalking";
+        internal const string isRunning = "isRunning";
+        internal const string doHurtEnemy = "doHurtEnemy";
+        internal const string doLayEgg = "doLayEgg";
+        internal const string doAttack = "doAttack";
+    }
+    
+
+    public override void Start()
+    {
+        self = this;
+        SCP682Objects.Add(gameObject);
+        InitialState = new WanderState();
+        if (enemyType.isOutsideEnemy)
+        {
+            var scale = 4f;
+            gameObject.transform.Find("CrocodileModel").localScale = new(scale, scale, scale);
+        }
+        // agent.radius = 0.5f;
+        base.Start();
+    }
+    public override void OnCollideWithPlayer(Collider other)
+    {
+        base.OnCollideWithPlayer(other);
+        if (ActiveState is AttackPlayerState state)
+            state.AttackCollideWithPlayer(other);
+    }
+
+    public override void HitEnemy(
+        int force = 1,
+        PlayerControllerB? playerWhoHit = null,
+        bool playHitSFX = false,
+        int hitID = -1
+    )
+    {
+        base.HitEnemy(force, playerWhoHit, playHitSFX, hitID);
+        if (isEnemyDead)
+            return;
+
+        enemyHP -= force;
+        if (enemyHP <= 0 && !isEnemyDead)
+        {
+            // Our death sound will be played through creatureVoice when KillEnemy() is called.
+            // KillEnemy() will also attempt to call creatureAnimator.SetTrigger("KillEnemy"),
+            // so we don't need to call a death animation ourselves.
+
+            // We need to stop our search coroutine, because the game does not do that by default.
+            StopCoroutine(searchCoroutine);
+            KillEnemyOnOwnerClient();
+        }
+
+        if (ActiveState is not AttackPlayerState)
+            OverrideState(new AttackPlayerState());
+    }
+    
+
+    private class WanderState : AIBehaviorState
+    {
+        public override List<AIStateTransition> Transitions { get; set; } =
+            [new LayingEgg().ArrivedAtShipTransition(), new SawPlayer()];
+
+        public override void OnStateEntered(Animator creatureAnimator)
+        {
+            creatureAnimator.SetBool(Anim.isWalking, true);
+            self.SetDestinationToPosition(RoundManager.Instance.outsideAINodes[
+                RandomNumberGenerator.GetInt32(RoundManager.Instance.outsideAINodes.Length)].transform.position);
+        }
+
+        public override void AIInterval(Animator creatureAnimator)
+        {
+            
+        }
+
+        public override void OnStateExit(Animator creatureAnimator)
+        {
+            creatureAnimator.SetBool(Anim.isWalking, false);
+        }
+       
+    }
+
+    private class LayingEgg : AIBehaviorState
+    {
+        public override List<AIStateTransition> Transitions { get; set; } =
+            [];
+        //TODO Transition must be done through animation toward walking state
+
+        public override void OnStateEntered(Animator creatureAnimator)
+        {
+
+        }
+
+        public override void OnStateExit(Animator creatureAnimator)
+        {
+
+        }
+        //DONE
+        internal class ArrivedAtDestination : AIStateTransition
+        {
+            public override bool CanTransitionBeTaken()
+            {
+                //IfIsClose enough to destination
+                if (Vector3.Distance(self.agent.destination, self.transform.position)<1f)
+                    return true;
+                return false;
+            }
+
+            public override AIBehaviorState NextState()
+            {
+                if (self.isOutside)
+                    return new WanderState();
+                else
+                    return new LayingEgg();
+            }
+        }
+    }
+
+    private class DetectedPlayer : AIBehaviorState
+    {
+        // Note: We add one more transition to this afterwards!
+        public override List<AIStateTransition> Transitions { get; set; } =
+            [new SawPlayer()];
+
+        public override void OnStateEntered(Animator creatureAnimator)
+        {
+
+        }
+
+        public override void AIInterval(Animator creatureAnimator)
+        {
+
+        }
+
+        public override void OnStateExit(Animator creatureAnimator)
+        {
+            
+        }
+
+        public class CheckIfPlayerVisible : AIStateTransition
+        {
+
+            public override bool CanTransitionBeTaken()
+            {
+                if (self.CheckLineOfSightForPlayer())
+                {
+                    
+                }
+            }
+
+            public override AIBehaviorState NextState()
+            {
+                
+            }
+        }
+    }
+    
+
+    private class AtFacilityWanderingState : AIBehaviorState
+    {
+        public override List<AIStateTransition> Transitions { get; set; } =
+            [
+                new BoredOfFacilityTransition(),
+                new AtFacilityEatNoisyJesterState.FindNoisyJesterTransition(),
+                new SawPlayer()
+            ];
+
+        public override void OnStateEntered(Animator creatureAnimator)
+        {
+            creatureAnimator.SetBool(Anim.isMoving, true);
+
+            self.StartSearch(self.transform.position);
+        }
+
+        public override void OnStateExit(Animator creatureAnimator)
+        {
+            self.StopSearch(self.currentSearch);
+        }
+
+        private class BoredOfFacilityTransition : AIStateTransition
+        {
+            float debugMSGTimer = defaultBoredOfWanderingFacilityTimer;
+
+            public override bool CanTransitionBeTaken()
+            {
+                self.boredOfWanderingFacilityTimer -= Time.deltaTime;
+                if (debugMSGTimer - self.boredOfWanderingFacilityTimer > 1)
+                {
+                    debugMSGTimer = self.boredOfWanderingFacilityTimer;
+                    self.LogDebug(
+                        $"[{nameof(BoredOfFacilityTransition)}] Time until bored: {self.boredOfWanderingFacilityTimer}"
+                    );
+                }
+                if (self.boredOfWanderingFacilityTimer <= 0)
+                {
+                    return true;
+                }
+                else
+                    return false;
+            }
+
+            public override AIBehaviorState NextState()
+            {
+                self.boredOfWanderingFacilityTimer = defaultBoredOfWanderingFacilityTimer;
+                self.currentTravelDirection = TravelingTo.Ship;
+                return new WanderThroughEntranceState();
+            }
+        }
+    }
+    
+
+    private class AtFacilityEatNoisyJesterState : AIBehaviorState
+    {
+        public override List<AIStateTransition> Transitions { get; set; } =
+            [new NoisyJesterEatenTransition()];
+
+        public override void OnStateEntered(Animator creatureAnimator)
+        {
+            creatureAnimator.SetBool(Anim.isMoving, true);
+            self.SetAgentSpeed(Speed.Running);
+        }
+
+        public override void AIInterval(Animator creatureAnimator)
+        {
+            if (self.targetJester is null)
+                return;
+
+            var jesterPos = self.targetJester.agent.transform.position;
+
+            if (Vector3.Distance(jesterPos, self.transform.position) < 3)
+                self.targetJester.KillEnemy(true);
+
+            self.SetDestinationToPosition(jesterPos);
+        }
+
+        public override void OnStateExit(Animator creatureAnimator)
+        {
+            self.SetAgentSpeed(Speed.Walking);
+        }
+
+        internal class FindNoisyJesterTransition : AIStateTransition
+        {
+            public override bool CanTransitionBeTaken()
+            {
+                if (self.isOutside)
+                    return false;
+
+                for (int i = 0; i < JesterListHook.jesterEnemies.Count; i++)
+                {
+                    var jester = JesterListHook.jesterEnemies[i];
+                    if (jester is null)
+                    {
+                        JesterListHook.jesterEnemies.RemoveAt(i);
+                        i--;
+                        continue;
+                    }
+                    if (jester.farAudio.isPlaying)
+                    {
+                        self.targetJester = jester;
+                        JesterListHook.jesterEnemies.RemoveAt(i);
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            public override AIBehaviorState NextState() => new AtFacilityEatNoisyJesterState();
+        }
+
+        private class NoisyJesterEatenTransition : AIStateTransition
+        {
+            public override bool CanTransitionBeTaken() => self.targetJester is null;
+
+            public override AIBehaviorState NextState() => new AtFacilityWanderingState();
+        }
+    }
+
+
+    private class InvestigatePlayerState : AIBehaviorState
+    {
+        public override List<AIStateTransition> Transitions { get; set; } =
+            [new AttackPlayerTransition(), new LostPlayerTransition()];
+
+        public override void OnStateEntered(Animator creatureAnimator)
+        {
+            creatureAnimator.SetBool(Anim.isMoving, true);
+        }
+
+        public override void AIInterval(Animator creatureAnimator)
+        {
+            self.targetPlayer = self.FindNearestPlayer();
+            self.SetDestinationToPosition(self.targetPlayer.transform.position);
+        }
+
+        public override void OnStateExit(Animator creatureAnimator) { }
+    }
+
+    private class AttackPlayerState : AIBehaviorState
+    {
+        public override List<AIStateTransition> Transitions { get; set; } =
+            [new LostPlayerTransition()];
+
+        const float defaultCooldown = 0.5f;
+        float attackCooldown = defaultCooldown;
+
+        public override void OnStateEntered(Animator creatureAnimator)
+        {
+            creatureAnimator.SetBool(Anim.isMoving, true);
+            self.SetAgentSpeed(Speed.Running);
+        }
+
+        public override void UpdateBehavior(Animator creatureAnimator)
+        {
+            attackCooldown -= Time.deltaTime;
+        }
+
+        public override void AIInterval(Animator creatureAnimator)
+        {
+            self.targetPlayer = self.FindNearestPlayer();
+            self.SetDestinationToPosition(self.targetPlayer.transform.position);
+        }
+
+        public override void OnStateExit(Animator creatureAnimator)
+        {
+            self.SetAgentSpeed(Speed.Walking);
+        }
+
+        internal void AttackCollideWithPlayer(Collider other)
+        {
+            if (attackCooldown > 0)
+                return;
+
+            PlayerControllerB? player = self.MeetsStandardPlayerCollisionConditions(other);
+            if (player is not null)
+            {
+                int damageToDeal;
+                if (player.health > 45) // At min do 15 damage
+                    damageToDeal = player.health + 30; // Set health to 30
+                else
+                    damageToDeal = 15;
+                player.DamagePlayer(damageToDeal);
+                self.creatureAnimator.SetTrigger(Anim.doBite);
+
+                attackCooldown = defaultCooldown;
+            }
+        }
+    }
+    
+
+    private class AttackPlayerTransition : AIStateTransition
+    {
+
+        public override bool CanTransitionBeTaken()
+        {
+            aggressionTimer -= Time.deltaTime;
+            if (aggressionTimer <= 0)
+                return true;
+            return false;
+        }
+        public override AIBehaviorState NextState() => new AttackPlayerState();
+    }
+    public void SetLocationState(bool toOutside)
+    {
+        LogDebug("Set location state to OUTSIDE?: " + toOutside);
+        if (toOutside)
+        {
+            // Maybe using MyValidState was a mistake?
+            // Just results in writing more code without really advantages.
+            MyValidState = PlayerState.Outside;
+            SetEnemyOutside(true);
+        }
+        else
+        {
+            MyValidState = PlayerState.Inside;
+            SetEnemyOutside(false);
+        }
+    }
+
+    [ClientRpc]
+    public void TeleportSelfToOtherEntranceClientRpc(bool wasInside)
+    {
+        TeleportSelfToOtherEntrance(!wasInside);
+    }
+
+    private void TeleportSelfToOtherEntrance(bool isOutside)
+    {
+        var targetEntrance = RoundManager.FindMainEntranceScript(!isOutside);
+
+        Vector3 navMeshPosition = RoundManager.Instance.GetNavMeshPosition(
+            targetEntrance.entrancePoint.position
+        );
+        if (IsOwner)
+        {
+            agent.enabled = false;
+            transform.position = navMeshPosition;
+            agent.enabled = true;
+        }
+        else
+            transform.position = navMeshPosition;
+
+        serverPosition = navMeshPosition;
+        SetLocationState(!isOutside);
+
+        PlayEntranceOpeningSound(targetEntrance);
+    }
+
+    public void PlayEntranceOpeningSound(EntranceTeleport entrance)
+    {
+        if (entrance.doorAudios == null || entrance.doorAudios.Length == 0)
+            return;
+        entrance.entrancePointAudio.PlayOneShot(entrance.doorAudios[0]);
+        WalkieTalkie.TransmitOneShotAudio(entrance.entrancePointAudio, entrance.doorAudios[0]);
+    }
+
+    #endregion
+    #region Debug Stuff
+#if DEBUG
+    public static IEnumerator DrawPath(LineRenderer line, NavMeshAgent agent)
+    {
+        if (!agent.enabled)
+            yield break;
+        yield return new WaitForEndOfFrame();
+        line.SetPosition(0, agent.transform.position); //set the line's origin
+
+        line.positionCount = agent.path.corners.Length; //set the array of positions to the amount of corners
+        for (var i = 1; i < agent.path.corners.Length; i++)
+        {
+            line.SetPosition(i, agent.path.corners[i]); //go through each corner and set that to the line renderer's position
+        }
+    }
+
+
+
+    class DebugNewSearchRoutineAction(SCP682AI self) : MMButtonAction("New Search Routine")
+    {
+        protected override void OnClick()
+        {
+            self.StopSearch(self.currentSearch);
+            self.StartSearch(self.transform.position);
+        }
+    }
+
+    class DebugOverrideState(SCP682AI self, Type state) : MMButtonAction($"To {state.Name}")
+    {
+        protected override void OnClick()
+        {
+            var stateInstance = (AIBehaviorState)Activator.CreateInstance(state);
+            self.OverrideState(stateInstance);
+        }
+    }
+
+#endif
+    #endregion
+}
